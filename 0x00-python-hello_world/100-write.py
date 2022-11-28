e613848059f0023788c8936049dde893e4047169
abenezerayalew <aben.man.unity@gmail.com>
2022-11-28 14:17:34 -0800

    fix

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 import sys
-write = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-sys.stdout.write(write)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
