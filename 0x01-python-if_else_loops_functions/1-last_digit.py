8dbe190982e7db884409fa847686ded81e0a71f6
abenezerayalew <aben.man.unity@gmail.com>
2022-11-29 12:01:25 -0800

    bigchange

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 mod_num = number % 10 if number >= 0 else ((-number % 10) * -1)
 msg = f"Last digit of {number} is {mod_num}"
-if mod_num > 5:
-    print(f"{mgs}and is greater than 5")
+if mod_num > 5 and mod_num % 10 != 0:
+    print(f"{msg} and is greater than 5")
 elif mod_num == 0:
-    print(f"{msg}and is 0")
+    print(f"{msg} and is 0")
 
 else:
     print(f"{msg} and is less than 6 and not 0")
