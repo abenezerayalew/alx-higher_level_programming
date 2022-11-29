71ed1107d20498fb839b30c5c64992d2c0cef399
abenezerayalew <aben.man.unity@gmail.com>
2022-11-29 11:22:31 -0800

    zero_fix

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,7 +5,7 @@ number = random.randint(-10, 10)
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
-    print(f"{number} is is zero")
+    print(f"{number} is zero")
 elif number < 0:
     print(f"{number} is negative")
 else:
