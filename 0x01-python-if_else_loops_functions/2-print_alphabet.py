5a5fb843dc80ff2e45859d6e21fcd34e4828b529
abenezerayalew <aben.man.unity@gmail.com>
2022-11-29 12:05:00 -0800

    3rd

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-import random
-mod = 0
-number = random.randint(-10000, 10000)
-mod = number % 10
-if mod > 5:
-    print(f"Last digit of {number} is {mod} and is greater than 5")
-elif mod == 0:
-    print(f"Last digit of {number} is {mod} and is 0")
-
-elif mod < 6:
-    print(f"Last digit of {number} is {mod} and is less than 6 and not 0")
-else:
-    print("error")
+for letter in range(97,123):
+    print(chr(letter), end = "")
