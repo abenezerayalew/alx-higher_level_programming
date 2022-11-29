5d3210c69b3d5df2362849d466ee1c29231f0f21
abenezerayalew <aben.man.unity@gmail.com>
2022-11-29 12:42:57 -0800

    6th

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for num in range(00, 100):
-    if (num != 100 and num != 0):
-        print(", ", end = "")
-    print("{:02d}".format(num), end = "")
+for num in range(100):
+    if (num != 99 and num != 0):
+        print("{}{}, ".format(int(num / 10), num % 10), end="")
+    else:
+        print("{}{}".format(int(num / 10), num % 10))
