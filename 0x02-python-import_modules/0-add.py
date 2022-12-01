e1112007a283a3df1acb8a3ed31016fe006b15a1
abenezerayalew <aben.man.unity@gmail.com>
2022-12-01 08:21:40 -0800

    takeThis

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == __main__
+    from add_0 import add
 #add = __import__('add_0').add
-a = 1
-b = 2
-result = add(a, b)
-print(f"{a} + {b} = {result}")
+    a = 1
+    b = 2
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
