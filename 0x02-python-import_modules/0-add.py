7f4be8cd85b11babbd70dda10e0432de590fe949
abenezerayalew <aben.man.unity@gmail.com>
2022-12-01 08:24:14 -0800

    plstakeThis

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,5 +4,4 @@ if __name__ == __main__
 #add = __import__('add_0').add
     a = 1
     b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print("{} + {} = {}".format(a, b, add(a, b))
