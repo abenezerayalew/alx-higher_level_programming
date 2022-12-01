0829cebf11650fb07986021fc033c9ebf2cefa8d
abenezerayalew <aben.man.unity@gmail.com>
2022-12-01 07:13:16 -0800

    second

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+add = add(a,b)
+sub = sub(a,b)
+mul = mul(a,b)
+div = div(a,b)
+print(f"{a} + {b} = {add}")
+print(f"{a} - {b} = {sub}")
+print(f"{a} * {b} = {mul}")
+print(f"{a} / {b} = {div}")
