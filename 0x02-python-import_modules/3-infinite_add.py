76a6b4d6f4848d6c8f39fb247909e061b9de6e5f
abenezerayalew <aben.man.unity@gmail.com>
2022-12-01 11:40:37 -0800

    FIXcodestyle

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    #num = len(sys.argv) - 1
-    #if num == 0:
-     #   print("{}".format(num))
-        
     value = 0
     for num in sys.argv:
         if num != sys.argv[0]:
