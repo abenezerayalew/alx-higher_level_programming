adcbf87d1957c3719d658e4f358c75530de216aa
abenezerayalew <aben.man.unity@gmail.com>
2023-02-15 16:57:38 +0300

    6th

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+--  A script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+
+CREATE DATABASE hbtn_0d_usa;
+CREATE TABLE states(id MEDIUMINT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
