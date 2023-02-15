d471ba211ec6ffe48bbb7d1af27365a9706622b9
abenezerayalew <aben.man.unity@gmail.com>
2023-02-15 16:29:30 +0300

    fix

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- A script that creates the database hbtn_0d_2 and the user user_0d_2. 
 
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT CREATE, SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
