bfa31e523d7d6b85779baa69a84f40db45e97a48
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 18:04:43 +0300

    task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
-//script that prints a square
+// script that prints a square
 
-const { argv } = require("process");
+const { argv } = require('process');
 
-let size = parseInt(argv[2]);
-if (isNaN(size))
-{
-    console.log("Missing size");
-}
-else
-{
-    let i = 0
-    while (i < size)
-    {
-    console.log("X".repeat(size));
+const size = parseInt(argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  while (i < size) {
+    console.log('X'.repeat(size));
     i++;
-    }
+  }
 }
