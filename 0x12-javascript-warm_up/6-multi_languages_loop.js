67624223868aa822e35a5644de187c2c8854638a
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 18:07:15 +0300

    task6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-//script that prints 3 lines: (like 1-multi_languages.js) 
-//but by using an array of string and a loop
-let lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+// script that prints 3 lines: (like 1-multi_languages.js)
+// but by using an array of string and a loop
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let index = 0;
-while (index < 3)
-{
-    console.log(lang[index]);
-    index++;
+while (index < 3) {
+  console.log(lang[index]);
+  index++;
 }
