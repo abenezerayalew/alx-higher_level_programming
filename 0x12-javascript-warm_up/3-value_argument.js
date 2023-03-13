602b39bf51ec8c007493f29930e8189324964672
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 21:19:26 +0300

    task3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // script that prints the first argument passed to it
-const { argv } = require('node:process');
 
 if (typeof process.argv[2] === 'undefined') {
   console.log('No argument');
