a17f6d0f1e84e2892c86310da8ac9224a001bc61
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 17:52:00 +0300

    task10fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,22 +1,16 @@
 #!/usr/bin/node
 
-const { argv } = require("process")
+const { argv } = require('process');
 
 // script that computes and prints a factorial
-num = parseInt(argv[2]);
-function factorial(num) 
-{
-    if (isNaN(num) || num === 0)
-    {
-        return 1;
-    }
-    else if (num < 0)
-    {
-        return -1;
-    }
-    else
-    {
-        return (num * factorial(num - 1));
-    }
+const num = parseInt(argv[2]);
+function factorial (num) {
+  if (isNaN(num) || num === 0) {
+    return 1;
+  } else if (num < 0) {
+    return -1;
+  } else {
+    return (num * factorial(num - 1));
+  }
 }
 console.log(factorial(num));
