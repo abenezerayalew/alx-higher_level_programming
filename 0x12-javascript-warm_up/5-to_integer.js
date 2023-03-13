c10d474966599338e75bfe0117fd4a24365fa6d3
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 18:08:11 +0300

    task5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 
-const { argv } = require("process")
+const { argv } = require('process');
 
-//script that prints My number: <first argument converted in integer> 
+// script that prints My number: <first argument converted in integer>
 // if the first argument can be converted to an integer
-let num = parseInt(argv[2]);
+const num = parseInt(argv[2]);
 
-if (isNaN(num))
-{
-    console.log("Not a number");
-}
-else
-{
-    console.log("My number: " + num);
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
 }
