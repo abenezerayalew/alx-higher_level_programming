d5d16421382aca18c5572e9c87bd7488294cc1fa
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 17:36:43 +0300

    fix

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 // function that increments and calls a function.
-exports.addMeMaybe =  function (number, theFunction)
-{
-    number++;
-    theFunction(number);
-}
+exports.addMeMaybe = function (number, theFunction) {
+  number++;
+  theFunction(number);
+};
