4d6c6a2ce31935fb9693a9a57ffe015c5ce05841
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 18:05:53 +0300

    task7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
 
-const { argv } = require("process");
+const { argv } = require('process');
 
-//script that prints x times “C is fun”
-let x = parseInt(argv[2]);
+// script that prints x times “C is fun”
+const x = parseInt(argv[2]);
 
-if (isNaN(x))
-{
-    console.log("Missing number of occurrences");
-}
-else
-{
-    let i = 0;
-    while (i < x)
-    {
-        console.log("C is fun");
-        i++;
-    }
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
 }
