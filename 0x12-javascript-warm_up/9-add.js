cbe45b3b8a6bd4adb3d6fc4494b7e0cb527b66db
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 18:03:00 +0300

    task9fix

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
-
+// script that prints the addition of 2 integers
 const { argv } = require('process');
 
 // script that prints the addition of 2 integers
 const num1 = parseInt(argv[2]);
 const num2 = parseInt(argv[3]);
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('NaN');
-}
+
 function add (a, b) {
   return a + b;
 }
-
-console.log(add(num1, num2));
+if (isNaN(num1) || isNaN(num2)) {
+  console.log(NaN);
+} else {
+  console.log(add(num1, num2));
+}
