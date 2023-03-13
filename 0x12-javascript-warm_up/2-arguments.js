3ec78adab2ac20bf14a52e9e476630fd4a30e446
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 14:30:37 +0300

    fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-//script that prints a message depending of the number of arguments passed
-const { argv } = require('process');
 
-if (process.argv.length == 2)
-{
-    console.log("No argument");
-}
-else if (process.argv.length == 3)
-{
-    console.log("Argument found");
-}
-else
-{
-    console.log("Argument found");
+const { argv } = require('process');
+const argc = argv.length;
+
+if (argc === 2) {
+  console.log('No argument');
+} else if (argc === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
