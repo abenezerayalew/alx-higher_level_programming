3be3c420cebe49bb56e30035dcf3eb05bd599858
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 18:09:05 +0300

    task4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const { argv } = require("process");
+const { argv } = require('process');
 
-//script that prints two arguments passed to it, in the following format: “ is ”
+// script that prints two arguments passed to it, in the following format: “ is ”
 console.log(argv[2] + ' is ' + argv[3]);
