5e40390e76c92ad11d376334967e2edca991e0bc
abenezerayalew <aben.man.unity@gmail.com>
2023-03-13 22:17:24 +0300

    test2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,8 @@
 const { argv } = require('process');
 const argc = argv.length;
 
-if (argc === 2) {
+if (argc <= 2) {
   console.log('No argument');
-} else if (argc === 3) {
-  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
