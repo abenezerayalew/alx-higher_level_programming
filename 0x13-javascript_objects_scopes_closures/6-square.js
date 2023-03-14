7ddc9cbd95647f969b7add02c74924bc5165af81
abenezerayalew <aben.man.unity@gmail.com>
2023-03-14 16:04:14 +0300

    test6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+// class Square that defines a square and inherits from Square
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
+  constructor (size) {
+    super(size);
+    this.size = size;
+  }
+
+  charPrint (c = 'X') {
+    if (typeof c === 'undefined') {
+      return c === 'X';
+    }
+    let i = 0;
+    while (i < this.size) {
+      console.log(c.repeat(this.size));
+      i++;
+    }
+  }
+}
+module.exports = Square;
