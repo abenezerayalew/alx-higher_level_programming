cac86ab6c318f268d9649341b48bd6d00ba37f3b
abenezerayalew <aben.man.unity@gmail.com>
2023-03-14 17:17:12 +0300

    update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,18 +3,13 @@
 const SquareP = require('./5-square');
 
 class Square extends SquareP {
-  constructor (size) {
-    super(size);
-    this.size = size;
-  }
-
   charPrint (c = 'X') {
     if (typeof c === 'undefined') {
       c = 'X';
     }
     let i = 0;
-    while (i < this.size) {
-      console.log(c.repeat(this.size));
+    while (i < this.height) {
+      console.log(c.repeat(this.width));
       i++;
     }
   }
