50896bdc723842042801fefefb040006aab1a744
abenezerayalew <aben.man.unity@gmail.com>
2023-03-18 23:35:07 +0300

    test1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
                 passwd=db_passwd,
                 db=db_name)
     cursor = database.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+    cursor.execute("SELECT id, name FROM states\
             ORDER BY states.id ASC")
     for row in cursor.fetchall():
         print(row)
