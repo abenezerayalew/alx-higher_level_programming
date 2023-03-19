27f0ef1962b8478b6657c46e104fbbd5ddb058f5
abenezerayalew <aben.man.unity@gmail.com>
2023-03-20 01:51:55 +0300

    task1

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     db_user = argv[1]
     db_passwd = argv[2]
     db_name = argv[3]
-    search = '{}'.format(argv[4])
+    search_state = '{}'.format(argv[4])
 
     database = MySQLdb.connect(host='localhost',
                                port=3306,
@@ -23,6 +23,6 @@ if __name__ == '__main__':
                    JOIN states\
                    ON cities.state_id = states.id\
                    WHERE states.name = %s\
-                   ORDER BY states.id ASC', (search,))
+                   ORDER BY states.id ASC', (search_state,))
     for row in cursor.fetchall():
         print(row)
