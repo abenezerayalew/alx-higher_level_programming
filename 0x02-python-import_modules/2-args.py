c01611eec10428897cd75cc14b8622fb1ccb9445
abenezerayalew <aben.man.unity@gmail.com>
2023-03-23 23:44:35 +0300

    opt

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    num = len(sys.argv) - 1
-    if num == 0:
-        print("{} arguments.".format(num))
-    elif (num == 1):
-        print("{} argument:".format(num))
+    from sys import argv
+    length = len(argv)-1
+    if length == 0:
+        print("{} arguments.".format(length))
+    elif length == 1:
+        print("{} argument:".format(length))
+        print("{}: {}".format(length, argv[length]))
     else:
-        print("{} arguments:".format(num))
-    if num >= 1:
-        num = 0
-        for y in sys.argv:
-            if num != 0:
-                print("{}: {}".format(num, y))
-            num = num + 1
+        print("{} arguments:".format(length))
+        for num in range(1, len(argv)):
+            print("{}: {}".format(num, argv[num]))
