de93876f5e9062dc2fdb6bf2e3c5984b22c1af19
abenezerayalew <aben.man.unity@gmail.com>
2023-03-25 13:21:58 +0300

    optimize

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 from sys import argv
-from calculator_1 import add, mul, sub, div
+
 length = len(argv)-1
 if length != 3:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     exit(1)
 else:
-    a = int(argv[1])
-    b = int(argv[3])
     operator = argv[2]
     if operator != '+' and operator != '-' and operator != '*' and operator != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+    from calculator_1 import add, mul, sub, div
+    a = int(argv[1])
+    b = int(argv[3])
+    
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == '-':
