fbe70137b1dc497ab05d9bc1c9410e271a3e457b
abenezerayalew <aben.man.unity@gmail.com>
2023-04-25 15:10:28 +0300

    task4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// script that prints the number of movies
+//  where the character “Wedge Antilles” is present
+const { argv } = require('process');
+const request = require('request');
+const api = argv[2];
+// const id =  argv[3]
+request(api, (err, response, body) => {
+  if (err) throw err;
+  else {
+    const id = JSON.parse(body).results.filter((elem) => {
+      return elem.characters.filter((url) => { return url.includes('18'); }).length;
+    }).length;
+    console.log(id);
+  }
+});
