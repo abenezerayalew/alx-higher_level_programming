3771fcdedbda4c43046715110ee9d150512f50f0
abenezerayalew <aben.man.unity@gmail.com>
2023-04-25 14:14:02 +0300

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// script that prints the title of a Star Wars movie 
+// script that prints the title of a Star Wars movie
 // where the episode number matches a given integer
 const { argv } = require('process');
 const request = require('request');
-let api = 'https://swapi-api.alx-tools.com/api/films/' + argv[2]
+const api = 'https://swapi-api.alx-tools.com/api/films/' + argv[2];
 request(api, (err, response, body) => {
   if (err) throw err;
   else {
