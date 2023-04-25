1bac2d38f4dd2e404c890b47ca7acfc55fedc932
abenezerayalew <aben.man.unity@gmail.com>
2023-04-25 12:50:45 +0300

    0task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 // script that reads and prints the content of a file.
 const { argv } = require('process');
 
-    const fs = require('fs')
-    fs.readFile(argv[2], 'utf-8',(err, inputD) => {
-       if (err) throw err;
-          console.log(inputD.toString());
-    })
+const fs = require('fs');
+fs.readFile(argv[2], 'utf-8', (err, input) => {
+  if (err) throw err;
+  console.log(input.toString());
+});
