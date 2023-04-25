6784fa448fcfcd6c3cd4698015e7a80b243ec171
abenezerayalew <aben.man.unity@gmail.com>
2023-04-25 13:01:24 +0300

    1task

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that writes a string to a file.
+const { argv } = require('process');
+
+const fs = require('fs');
+const fInput = argv[3];
+fs.writeFile(argv[2], fInput, 'utf-8', (err) => {
+  if (err) throw err;
+  else {
+    console.log('The file is updated with the given data');
+  }
+});
